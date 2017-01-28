7d0b409a3c71828007915120dd09a8ee301c55e9
Rodrigue Mouadeu <rodrigue.mouadeu@gmail.com>
2017-01-27 21:43:53 -0500

    init

---
+++ hello-vaadin/src/com/rodriguemouadeu/hellovaadin/HelloVaadinApplication.java
@@ -0,0 +1,20 @@
+package com.rodriguemouadeu.hellovaadin;
+
+import com.vaadin.Application;
+import com.vaadin.ui.*;
+
+/**
+ * Main application class.
+ */
+public class HelloVaadinApplication extends Application {
+
+	@Override
+	public void init() {
+		Window mainWindow = new Window("Hello Vaadin Application");
+		Label label = new Label("Hello Vaadin user");
+		mainWindow.addComponent(label);
+		setMainWindow(mainWindow);
+	}
+
+}
+
